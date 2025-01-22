576da6fcfdf6344402620dc7ac5c5cc77063eeb2
sivaprasadnk <sivaprasadnk123@gmail.com>
2025-01-22 14:15:18 +0530

    adding url

--- lib/core/common_strings.dart
+++ lib/core/common_strings.dart
@@ -9,3 +9,8 @@ const kAppName = 'Fitness in Thoughts';
 const kPlayStoreIconAssetPath = 'assets/images/play_store_icon.png';
 
 String baseUrl = 'https://fitnessblogbackend-production.up.railway.app/';
+
+String webUrl = 'https://sivaprasadnk.dev/fitnessInThoughts/';
+
+String playStoreUrl =
+    'https://play.google.com/store/apps/details?id=com.sptpra.fitness_thoughts';
